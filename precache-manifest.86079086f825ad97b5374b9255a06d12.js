71946609a4fca7a4b21f541048b97f6a9d3d178c
unknown <bodoyanmyasnik96@gmail.com>
2020-04-23 10:58:46 +0400

    Updates

---
+++ precache-manifest.86079086f825ad97b5374b9255a06d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68e2a8b012132faa06a32a9a93eebf3",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "910702970cb3f7f7eea9",
+    "url": "/e-commerce/static/css/main.4f83ca72.chunk.css"
+  },
+  {
+    "revision": "afc359c47f7e831e0e8e",
+    "url": "/e-commerce/static/js/2.91da65db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/e-commerce/static/js/2.91da65db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910702970cb3f7f7eea9",
+    "url": "/e-commerce/static/js/main.38bbfd05.chunk.js"
+  },
+  {
+    "revision": "58e619211bbc53bcaebd",
+    "url": "/e-commerce/static/js/runtime-main.b675c991.js"
+  }
+]);
